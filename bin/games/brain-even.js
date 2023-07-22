26fbd086c7a1e77a79fd6b5c1ee9cdfdc9b237e8
Denis <denissalnikoff@mail.ru>
2023-07-22 21:24:15 +0400

    fix bin/games/brain-even.js

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -3,11 +3,11 @@ import { greeting, askQuestion } from '../../src/cli.js';
 import { getRandomPositiveInt } from '../../src/math-utils.js';
 
 const name = greeting();
-const maxNumber = 1000;
+const maxNumber = 999;
 const winCount = 3;
+const condition = 'Answer "yes" if the number is even, otherwise answer "no".';
 let count;
 for (count = 0; count < winCount; count += 1) {
-  const condition = 'Answer "yes" if the number is even, otherwise answer "no".';
   const randomNumber = getRandomPositiveInt(maxNumber);
   const correctAnswer = randomNumber % 2 ? 'no' : 'yes';
   if (!askQuestion(condition, randomNumber, correctAnswer, name)) break;
