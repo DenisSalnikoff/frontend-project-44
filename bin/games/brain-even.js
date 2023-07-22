86a18f917e611c52c930b870b5963fdf0564496b
Denis <denissalnikoff@mail.ru>
2023-07-22 20:13:22 +0400

    fix: code restructuring

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,26 +1,16 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from '../../src/cli.js';
+import { greeting, askQuestion } from '../../src/cli.js';
 
 const getRandomPositiveInt = (max) => Math.floor(Math.random() * max) + 1;
 
 const name = greeting();
 const maxNumber = 1000;
 const winCount = 3;
-let count = 0;
-while (count < winCount) {
-  const number = getRandomPositiveInt(maxNumber);
-  const correctAnswer = number % 2 ? 'no' : 'yes';
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (answer.toLocaleLowerCase() === correctAnswer) {
-    console.log('Correct!');
-    count += 1;
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${name}!`);
-    break;
-  }
+let count;
+for (count = 0; count < winCount; count += 1) {
+  const condition = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const randomNumber = getRandomPositiveInt(maxNumber);
+  const correctAnswer = randomNumber % 2 ? 'no' : 'yes';
+  if (!askQuestion(condition, randomNumber, correctAnswer, name)) break;
 }
 if (count === 3) console.log(`Congratulations, ${name}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,21 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const greeting = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
+};
+
+export const askQuestion = (condition, question, correctAnswer, name) => {
+  console.log(condition);
+  console.log(`Question: ${question}`);
+  const answer = readlineSync.question('Your answer: ');
+  const correct = answer.trim().toLocaleLowerCase() === correctAnswer;
+  if (correct) {
+    console.log('Correct!');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${name}!`);
+  }
+  return correct;
 };
