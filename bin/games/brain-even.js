52a7eea1ed69bc3cbc8fbe961b3b6ec680b4a5c0
Denis <denissalnikoff@mail.ru>
2023-07-22 19:28:44 +0400

    fix brain-even: 3 rounds made

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -6,8 +6,9 @@ const getRandomPositiveInt = (max) => Math.floor(Math.random() * max) + 1;
 
 const name = greeting();
 const maxNumber = 1000;
-let winCount = 0;
-while (winCount < 3) {
+const winCount = 3;
+let count = 0;
+while (count < winCount) {
   const number = getRandomPositiveInt(maxNumber);
   const correctAnswer = number % 2 ? 'no' : 'yes';
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
@@ -15,11 +16,11 @@ while (winCount < 3) {
   const answer = readlineSync.question('Your answer: ');
   if (answer.toLocaleLowerCase() === correctAnswer) {
     console.log('Correct!');
-    winCount += 1;
+    count += 1;
   } else {
     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
     console.log(`Let's try again, ${name}!`);
-    winCount = 0;
+    break;
   }
 }
-console.log(`Congratulations, ${name}!`);
+if (count === 3) console.log(`Congratulations, ${name}!`);
