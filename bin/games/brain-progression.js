1cf394326c753bcb7a4d741a556e49eb78eeac44
Denis <denissalnikoff@mail.ru>
2023-07-23 12:57:47 +0400

    add bin/games/brain-progression.js

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import { greeting, askQuestion, congratulations } from '../../src/cli.js';
+import { getRandomPositiveInt, getProgressionArray } from '../../src/math-utils.js';
+
+const name = greeting();
+const maxStartNumber = 50;
+const maxStep = 10;
+const minStepCount = 5;
+const maxStepCount = 15;
+const winCount = 3;
+const condition = 'What number is missing in the progression?';
+let count;
+for (count = 0; count < winCount; count += 1) {
+  const startNumber = getRandomPositiveInt(maxStartNumber);
+  const step = getRandomPositiveInt(maxStep);
+  const stepCount = getRandomPositiveInt(maxStepCount - minStepCount + 1) + minStepCount - 1;
+  const progressionArray = getProgressionArray(startNumber, step, stepCount);
+  const deleteElementIndex = getRandomPositiveInt(stepCount) - 1;
+  const correctAnswer = progressionArray[deleteElementIndex];
+  progressionArray[deleteElementIndex] = '..';
+  if (!askQuestion(condition, progressionArray.join(' '), correctAnswer.toString(), name)) break;
+}
+if (count === winCount) congratulations();
--- src/math-utils.js
+++ src/math-utils.js
@@ -1,3 +1,8 @@
+const euclideanAlgorithm = (a, b) => {
+  if (!b) return a;
+  return euclideanAlgorithm(b, a % b);
+};
+
 export const getRandomPositiveInt = (max) => Math.floor(Math.random() * max) + 1;
 
 export const getRandomMathSign = () => {
@@ -13,9 +18,12 @@ export const getRandomMathSign = () => {
   }
 };
 
-const euclideanAlgorithm = (a, b) => {
-  if (!b) return a;
-  return euclideanAlgorithm(b, a % b);
-};
-
 export const getGCD = (x, y) => ((x >= y) ? euclideanAlgorithm(x, y) : euclideanAlgorithm(y, x));
+
+export const getProgressionArray = (startNumber, step, stepCount) => {
+  const result = [startNumber];
+  for (let i = 1; i < stepCount; i += 1) {
+    result.push(result.at(-1) + step);
+  }
+  return result;
+};
